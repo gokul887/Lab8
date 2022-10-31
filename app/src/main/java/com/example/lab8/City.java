5459ecc120805a291b8ccdae761f73ae59815d49
gokul887 <sivadasa@ualberta.ca>
2022-10-31 13:41:16 -0600

    fix: change to city class due to failing add test

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -1,20 +1,45 @@
 package com.example.lab8;
 
-public class City {
-
+/**
+ * This class defines a city. It defines a city {@link String } and a province {@link String }
+ */
+public class City implements Comparable{
     private String city;
     private String province;
 
-    public City(String city, String province){
+    /**
+     * @param city - stores city name
+     * @param province - stores province name
+     */
+    City(String city, String province){
         this.city = city;
         this.province = province;
     }
 
+    /**
+     * @return
+     * returns city name
+     */
     String getCityName(){
         return this.city;
     }
 
+    /**
+     * @return
+     * returns province name
+     */
     String getProvinceName(){
         return this.province;
+    }
+
+    /**
+     * @param o - object to compare {@link Object}
+     * @return
+     * returns comparison with object o
+     */
+    @Override
+    public int compareTo(Object o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName());
     }
 }
