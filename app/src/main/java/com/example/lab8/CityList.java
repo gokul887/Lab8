04f609b3f2e01e24951be9484a7e4da34cb2eb5d
gokul887 <sivadasa@ualberta.ca>
2022-10-31 13:31:28 -0600

    fix: changed source files because of build error and added hasCityTest

---
+++ app/src/main/java/com/example/lab8/CityList.java
@@ -0,0 +1,45 @@
+package com.example.lab8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a class that keeps track of a list of city objects.
+ */
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+
+    /**
+     * This adds a city to the list if the city does not exist
+     *
+     * @param city This is a candidate city to add
+     */
+    public void add(City city) {
+        if (cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
+    }
+
+    /**
+     * This returns a sorted list of cities
+     *
+     * @return Return the sorted list
+     */
+    public List getCities() {
+        List list = cities;
+        Collections.sort(list);
+        return list;
+    }
+
+    /**
+     * This returns if list has a city
+     *
+     * @param city This is a candidate city to test for existence
+     * @return Return whether list has a particular city
+     */
+    public boolean hasCity(City city) {
+        return false;
+    }
+}
---
+++ app/src/test/java/com/example/lab8/CityListTest.java
@@ -0,0 +1,50 @@
+package com.example.lab8;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CityListTest {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CityList cityList = mockCityList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+        assertThrows( IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
+
+    @Test
+    void testHasCity() {
+        CityList cityList = mockCityList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city);
+        assertTrue(cityList.hasCity(city));
+        City city2 = new City("Edmonton", "AB");
+        assertFalse(cityList.hasCity(city2));
+    }
+}
